b0ec7796b57a373935f71590efff2301c9217f37
Emilien C-Perron <emilien.cp@hotmail.com>
2018-10-06 12:12:59 -0400

    deplacer droite

--- src/main/java/io/polyhx/lhgames/game/bot/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/bot/Bot.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public class Bot extends BaseBot {
     public IAction getAction(Map map, Player player, List<Player> others, GameInfo info) {
-        return createMoveAction(Point.LEFT);
+        return createMoveAction(Point.RIGHT);
     }
 }
