b4a85855308acc8ed9c2bada9072df1b6ebbbaff
lcdem <lcdemers16@gmail.com>
2018-10-06 20:01:07 -0400

    correction5

--- src/main/java/io/polyhx/lhgames/game/bot/Bot.java
+++ src/main/java/io/polyhx/lhgames/game/bot/Bot.java
@@ -24,11 +24,83 @@ public class Bot extends BaseBot {
     Point pointVerifier=Point.UP;
     Point pointDeplacer=Point.RIGHT;
     public IAction getAction(Map map, Player player, List<Player> others, GameInfo info) {
+        /*Evenement event= Evenement.DEPLACER;
+        IAction action=null;
+        map.getTile(player.getX()+1, player.getY());
+        if(player.getCarriedResource() >= player.getResourceCapacity()){
+            event=vaVers(player);
+            pointVerifier=pointDeplacer;
+            event=regarderDevant(player,map);
+        }
+        else{
+            System.out.println("action faite2");
+        do{
 
+            event=regarderDevant(player, map);
+            if(event!=Evenement.DEPLACER){
+                break;
+            }
+            if(pointVerifier==Point.UP){
+                pointVerifier=Point.RIGHT;
+            }
+            else if(pointVerifier==Point.RIGHT){
+                pointVerifier=Point.DOWN;
+            }
+            else if(pointVerifier==Point.DOWN){
+                pointVerifier=Point.LEFT;
+            }
+            else if(pointVerifier==Point.LEFT){
+                pointVerifier=Point.UP;
+            }
+        }while(pointVerifier!=Point.UP);
+        Random rdn=new Random();
+        int aleatoire=rdn.nextInt()%4;
+            switch(aleatoire){
+                case 0:
+                    pointDeplacer=Point.RIGHT;
+                    break;
+                case 1:
+                    pointDeplacer=Point.DOWN;
+                    break;
+                case 2:
+                    pointDeplacer=Point.LEFT;
+                    break;
+                case 3:
+                    pointDeplacer=Point.UP;
+                    break;
+            }
+        }
+
+        System.out.println(event);
+        System.out.println(pointVerifier.getX()+" "+pointVerifier.getY());
+       switch (event) {
+            case VOLER:
+                action = createStealAction(pointVerifier);
+                break;
+            case DEPLACER:
+                action = createMoveAction(pointDeplacer);
+                break;
+            case ATTAQUER:
+                action = createMeleeAttackAction(pointVerifier);
+                break;
+            case RAMASSER:
+                action = createCollectAction(pointVerifier);
+                break;
+            case DETRUIRE:
+                action = createMeleeAttackAction(pointVerifier);
+                break;
+            case ACHETER:
+                action = createMoveAction(pointDeplacer);
+                break;
+            default:
+                break;
+        }
+        ancienneAction=action;*/
+        System.out.println("alloooo");
         return createMeleeAttackAction(Point.UP);
     }
 
-    public Evenement regarderDevant(Player player, Map map){
+    /*public Evenement regarderDevant(Player player, Map map){
         Point pointCase=new Point(player.getX()+pointVerifier.getX(),player.getY()+pointVerifier.getY());
         Tile tile = map.getTile(pointCase);
         if(tile.isResource())
@@ -57,13 +129,12 @@ public class Bot extends BaseBot {
     }
 
 
-    Evenement reviens(Player player)
+    Evenement vaVers(Player player, Point point)
     {
         Point positionActuelle = player.getPosition();
-        Point positionMaison = player.getHousePosition();
 
-        int decalageX = positionActuelle.getX() - positionMaison.getX();
-        int decalageY = positionActuelle.getY() - positionMaison.getY();
+        int decalageX = positionActuelle.getX() - point.getX();
+        int decalageY = positionActuelle.getY() - point.getY();
 
         Random deplacementAleatoire= new Random();
 
@@ -107,4 +178,32 @@ public class Bot extends BaseBot {
             }
         }
     }
+
+    Point trouverRessources(Map map, Player player)
+    {
+        int positionX = player.getPosition().getX();
+        int positionY = player.getPosition().getX();
+
+        Tile tilePréférée=null;
+        int distanceMinimale = 20;
+        for(int i=-10; i < 10; i++)
+        {
+            for(int j=-10; j < 10; j++)
+            {
+                int distance = Math.abs(i)+Math.abs(j);
+                Tile tileCourante = map.getTile(positionX+i, positionY+j);
+                if(!tileCourante.isEmpty() && !tileCourante.isLava() && !tileCourante.isWall())
+                {
+                    if(distance < distanceMinimale)
+                    {
+                        distanceMinimale = distance;
+                        tilePréférée = tileCourante;
+                    }
+                }
+            }
+        }
+
+        Point pointAaller = new Point(tilePréférée.getX(), tilePréférée.getY());
+        return pointAaller;
+    }*/
 }
